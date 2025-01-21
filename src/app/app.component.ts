cf9cead33e5524edb086347ed77dd8047970c118
ryanlu-dev <ryan@ryanlu.dev>
2025-01-21 03:59:49 -0500

    Complete website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { HeroSectionComponent } from './sections/hero/hero-section.component';
 import { AboutSectionComponent } from './sections/about/about-section.component';
 import { EducationSectionComponent } from './sections/education/education-section.component';
 import { ExperienceSectionComponent } from './sections/experience/experience-section.component';
+import { ProjectsSectionComponent } from './sections/projects/projects-section.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [HeroSectionComponent, AboutSectionComponent, EducationSectionComponent, ExperienceSectionComponent],
+  imports: [HeroSectionComponent, AboutSectionComponent, EducationSectionComponent, ExperienceSectionComponent, ProjectsSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/sections/about/about-section.component.ts
+++ src/app/sections/about/about-section.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { TooltipStrDirective } from 'ngx-tooltip-directives';
+
 @Component({
   selector: 'app-about-section',
   templateUrl: './about-section.component.html',
   styleUrls: ['./about-section.component.scss'],
+  imports: [TooltipStrDirective]
 })
 
 export class AboutSectionComponent {
