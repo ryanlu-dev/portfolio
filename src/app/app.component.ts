d38ed5db09c4de8b642c7516bb1a87b20fa06de8
ryanlu-dev <ryan@ryanlu.dev>
2025-01-22 17:57:09 -0500

    add footer section and fix spacing for education years

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { AboutSectionComponent } from './sections/about/about-section.component'
 import { EducationSectionComponent } from './sections/education/education-section.component';
 import { ExperienceSectionComponent } from './sections/experience/experience-section.component';
 import { ProjectsSectionComponent } from './sections/projects/projects-section.component';
+import { FooterSectionComponent } from './sections/footer/footer-section.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [HeroSectionComponent, AboutSectionComponent, EducationSectionComponent, ExperienceSectionComponent, ProjectsSectionComponent],
+  imports: [HeroSectionComponent, AboutSectionComponent, EducationSectionComponent, ExperienceSectionComponent, ProjectsSectionComponent, FooterSectionComponent] ,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
